1708434b93dff8f13a62d3ddf971111b37e554d2
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-07-19 17:47:10 +0200

    initialized webpack

--- public/index.js
+++ public/index.js
@@ -1,6 +1,107 @@
-'use strict';
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-const phones = [
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_phone_page__ = __webpack_require__(1);
+
+
+
+
+new __WEBPACK_IMPORTED_MODULE_0__components_phone_page__["a" /* default */]({
+  el: document.querySelector('[data-component="phone-page"]')
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__phone_catalogue__ = __webpack_require__(2);
+
+
+
+
+class PhonePage {
+  constructor(options) {
+    this._el = options.el;
+
+    this._catalogue = new __WEBPACK_IMPORTED_MODULE_0__phone_catalogue__["a" /* default */]({
+      el: this._el.querySelector('[data-component="phone-catalogue"]'),
+      phones: phonesFromServer
+    });
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = PhonePage;
+
+
+const phonesFromServer = [
   {
     "age": 0,
     "id": "motorola-xoom-with-wi-fi",
@@ -157,33 +258,35 @@ const phones = [
 ];
 
 
-class PhonesCatalogue {
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+
+class PhoneCatalogue {
   constructor(options) {
     this._el = options.el;
     this._phones = options.phones;
 
     const template = document.getElementById('phone-catalogue-template').innerHTML;
+
     this._compiledTemplate = _.template(template);
-    
-    
+
     this._render();
-    this._el.addEventListener('click', this._onPhoneClick.bind(this))
   }
 
-    _render() {
-      this._el.innerHTML = this._compiledTemplate({
-        phones: this._phones
-      });
-    }
-
-    _onPhoneClick(event) {
-        let phoneItemLink = event.target.closest('[data-element="phoneItemLink"]');
-
-        if (!phoneItemLink) {
-        return;
-        }
-
-        let selectedPhoneItem = phoneItemLink.closest('[data-element="phoneItem"]');
-        alert(selectedPhoneItem.dataset.phoneId);
-    }
+  _render() {
+    this._el.innerHTML = this._compiledTemplate({
+      phones: this._phones
+    });
+  }
 }
+/* harmony export (immutable) */ __webpack_exports__["a"] = PhoneCatalogue;
+
+
+
+/***/ })
+/******/ ]);
+
